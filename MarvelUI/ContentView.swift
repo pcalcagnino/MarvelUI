fa4e782573ea86489f2a6fe8aba790a04c5695c3
Pablo Calcagnino <pablo.calcagnino@globant.com>
2021-04-08 11:44:40 -0300

    Initial project creation

---
+++ MarvelUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MarvelUI
+//
+//  Created by Pablo Calcagnino on 08/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MarvelUI/MarvelUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelUIApp.swift
+//  MarvelUI
+//
+//  Created by Pablo Calcagnino on 08/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct MarvelUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
